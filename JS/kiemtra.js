a5635b77b494b687a5d5bd3c1c4cfac88b83da29
RamBo2k4 <nguyenvannguyen2552004@gmail.com>
2024-04-10 20:42:13 +0700

    Sing vs kiemtra

--- JS/kiemtra.js
+++ JS/kiemtra.js
@@ -35,4 +35,46 @@ function CheckMK(){
         document.getElementById("tbMK").innerHTML = "";
         return true;
     }
-}a
+}
+function CheckNLMK(){
+    let NLMK = document.getElementById("txtNLMK").value;
+    let MK = document.getElementById("txtMK").value;
+    if (NLMK.trim().length == 0) {
+        document.getElementById("tbNLMK").innerHTML = "Không để trống";
+        return false;
+    } else if (NLMK != MK) {
+        document.getElementById("tbNLMK").innerHTML = "Khác mật Khẩu";
+        return false;
+    } else {
+        return true;
+    }
+   
+}
+function CheckE(){
+    let regex = /^[a-zA-Z0-9._-]+@(gmail|email|outlook)\.com$/;
+    let E = document.getElementById("txtE").value;
+    if (E.trim().length == 0) {
+        document.getElementById("tbE").innerHTML = "Không để trống";
+        return false;
+    } else if (!(regex.test(E))) {
+        document.getElementById("tbE").innerHTML ="Nhập sai định dạnh";
+        return false;
+    } else {
+        document.getElementById("tbE").innerHTML = "";
+        return true;
+    }
+}
+function CheckSDT(){
+    let SDT = document.getElementById("txtSDT").value;
+    let regex = /^[0-9]$/;
+    if (SDT.trim().length == 0) {
+        document.getElementById("tbSDT").innerHTML = "Không để trống";
+        return false;
+    } else if (!(regex.test(SDT))) {
+        document.getElementById("tbSDT").innerHTML ="Nhập sai định dạnh";
+        return false;
+    } else {
+        document.getElementById("tbSDT").innerHTML = "";
+        return true;
+    }
+}
