d86c35cb972ed057f6e5ec3a98927e14a815a025
RamBo2k4 <nguyenvannguyen2552004@gmail.com>
2024-04-10 16:39:54 +0700

    up 1

---
+++ JS/kiemtra.js
@@ -0,0 +1,38 @@
+function CheckHT(){
+    let regex = /^[A-Z][a-z]*(\s+[A-Z][a-z]*)*$/;
+    let HT = document.getElementById("txtHT").value;
+    if (HT.trim().length == 0) {
+        document.getElementById("tbHT").innerHTML ="Không để trống";
+        return false;
+    } else if (!(regex.test(HT))) {
+        document.getElementById("tbHT").innerHTML ="Nhập sai định dạnh";
+        return false;
+    } else {
+        document.getElementById("tbHT").innerHTML ="";
+        return true;
+    }
+}
+function CheckDN(){
+    let DN = document.getElementById("txtDN").value;
+    if (DN.trim().length == 0) {
+        document.getElementById("tbDN").innerHTML = "Không để trống";
+        return false;
+    } else  {
+        document.getElementById("tbDN").innerHTML = "";
+        return true;
+    }
+}
+function CheckMK(){
+    let regex = /^[A-Z]{1}$/;
+    let MK = document.getElementById("txtMK").value;
+    if (MK.trim().length == 0) {
+        document.getElementById("tbMK").innerHTML = "Không để trống";
+        return false;
+    } else if (!(regex.test(MK))) {
+        document.getElementById("tbMK").innerHTML ="Nhập sai định dạnh";
+        return false;
+    } else {
+        document.getElementById("tbMK").innerHTML = "";
+        return true;
+    }
+}
